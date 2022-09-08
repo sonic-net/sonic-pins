77eb0791bbaacf274a9767ed45a77461fe504ee1
kishanps <kishanps@google.com>
2022-09-08 16:15:04 -0700

    [Thinkit] Change AppendSflowConfig to UpdateSflowConfig. Remove config speed changes using params as source of truth.

--- tests/sflow/sflow_util.cc
+++ tests/sflow/sflow_util.cc
@@ -14,11 +14,14 @@
 
 #include "tests/sflow/sflow_util.h"
 
+#include "absl/container/btree_map.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/strings/substitute.h"
 #include "absl/time/time.h"
 #include "gutil/status.h"
+#include "include/nlohmann/json.hpp"
 #include "lib/gnmi/gnmi_helper.h"
 #include "lib/utils/json_utils.h"
 #include "lib/validator/validator_lib.h"
@@ -170,7 +173,7 @@ absl::Status VerifySflowStatesConverged(
   return absl::OkStatus();
 }
 
-absl::StatusOr<std::string> AppendSflowConfig(
+absl::StatusOr<std::string> UpdateSflowConfig(
     absl::string_view gnmi_config, absl::string_view agent_addr_ipv6,
     const std::vector<std::pair<std::string, int>>& collector_address_and_port,
     const absl::flat_hash_set<std::string>& sflow_enabled_interfaces,
@@ -178,7 +181,7 @@ absl::StatusOr<std::string> AppendSflowConfig(
   ASSIGN_OR_RETURN(auto gnmi_config_json, json_yang::ParseJson(gnmi_config));
   if (agent_addr_ipv6.empty()) {
     return absl::InvalidArgumentError(
-        "loopback_address parameter cannot be empty.");
+        "agent_addr_ipv6 parameter cannot be empty.");
   }
   if (sflow_enabled_interfaces.empty()) {
     return absl::InvalidArgumentError(
@@ -196,36 +199,84 @@ absl::StatusOr<std::string> AppendSflowConfig(
   gnmi_config_json["openconfig-sampling:sampling"]
                   ["openconfig-sampling-sflow:sflow"]["config"]
                   ["agent-id-ipv6"] = agent_addr_ipv6;
-  // Sort collector IPs and interface names by string order to make sure the
-  // generated config has a determinisitic order.
-  std::vector<std::pair<std::string, int>> sorted_collector_address_and_port =
-      collector_address_and_port;
-  std::sort(sorted_collector_address_and_port.begin(),
-            sorted_collector_address_and_port.end());
-  for (const auto& [address, port] : sorted_collector_address_and_port) {
-    nlohmann::basic_json<> sflow_collector_config;
-    sflow_collector_config["address"] = address;
-    sflow_collector_config["port"] = port;
-    sflow_collector_config["config"]["address"] = address;
-    sflow_collector_config["config"]["port"] = port;
-    gnmi_config_json["openconfig-sampling:sampling"]
-                    ["openconfig-sampling-sflow:sflow"]["collectors"]
-                    ["collector"]
-                        .push_back(sflow_collector_config);
+  if (!collector_address_and_port.empty()) {
+    absl::btree_map<std::string, nlohmann::json>
+        collector_address_port_to_config_json;
+    for (const auto& [address, port] : collector_address_and_port) {
+      nlohmann::basic_json<> sflow_collector_config;
+      sflow_collector_config["address"] = address;
+      sflow_collector_config["port"] = port;
+      sflow_collector_config["config"]["address"] = address;
+      sflow_collector_config["config"]["port"] = port;
+      collector_address_port_to_config_json[absl::StrCat(address, ":", port)] =
+          sflow_collector_config;
+    }
+    nlohmann::json& collector_json_array =
+        gnmi_config_json["openconfig-sampling:sampling"]
+                        ["openconfig-sampling-sflow:sflow"]["collectors"]
+                        ["collector"];
+    if (!collector_json_array.empty() &&
+        collector_json_array.type() != nlohmann::json::value_t::array) {
+      return absl::InvalidArgumentError(
+          "json collector field already exists and is not an array.");
+    }
+
+    // Prune existing collector ip configs.
+    for (nlohmann::json& collector_json : collector_json_array) {
+      std::string address_and_port = absl::StrCat(
+          json_yang::GetSimpleJsonValueAsString(collector_json["address"]), ":",
+          json_yang::GetSimpleJsonValueAsString(collector_json["port"]));
+      auto it = collector_address_port_to_config_json.find(address_and_port);
+      if (it != collector_address_port_to_config_json.end()) {
+        collector_address_port_to_config_json.erase(it);
+      }
+    }
+
+    // Append collector ip config if any.
+    for (const auto& [address_and_port, config_json] :
+         collector_address_port_to_config_json) {
+      collector_json_array.push_back(config_json);
+    }
   }
-  absl::btree_set<std::string> sorted_interface_names(
-      sflow_enabled_interfaces.begin(), sflow_enabled_interfaces.end());
-  for (const auto& interface_name : sorted_interface_names) {
+
+  absl::btree_map<std::string, nlohmann::json> interface_name_to_config_json;
+  for (const auto& interface_name : sflow_enabled_interfaces) {
     nlohmann::basic_json<> sflow_interface_config;
     sflow_interface_config["name"] = interface_name;
-    sflow_interface_config["config"]["name"] = interface_name;
-    sflow_interface_config["config"]["enabled"] = true;
-    sflow_interface_config["config"]["ingress-sampling-rate"] = sampling_rate;
-    gnmi_config_json["openconfig-sampling:sampling"]
-                    ["openconfig-sampling-sflow:sflow"]["interfaces"]
-                    ["interface"]
-                        .push_back(sflow_interface_config);
+    sflow_interface_config["enabled"] = true;
+    sflow_interface_config["ingress-sampling-rate"] = sampling_rate;
+    interface_name_to_config_json[interface_name] = sflow_interface_config;
   }
+  nlohmann::json& interface_json_array =
+      gnmi_config_json["openconfig-sampling:sampling"]
+                      ["openconfig-sampling-sflow:sflow"]["interfaces"]
+                      ["interface"];
+  if (!interface_json_array.empty() &&
+      interface_json_array.type() != nlohmann::json::value_t::array) {
+    return absl::InvalidArgumentError(
+        "json interface field already exists and is not an array.");
+  }
+
+  // Modify all existing interface config.
+  for (nlohmann::json& interface_json : interface_json_array) {
+    std::string interface_name =
+        json_yang::GetSimpleJsonValueAsString(interface_json["name"]);
+    auto it = interface_name_to_config_json.find(interface_name);
+    if (it != interface_name_to_config_json.end()) {
+      interface_json["config"] = it->second;
+      interface_name_to_config_json.erase(it);
+    }
+  }
+
+  // Append interface config if any.
+  for (const auto& [interface_name, interface_config_json] :
+       interface_name_to_config_json) {
+    nlohmann::basic_json<> sflow_interface_json;
+    sflow_interface_json["name"] = interface_name;
+    sflow_interface_json["config"] = interface_config_json;
+    interface_json_array.push_back(sflow_interface_json);
+  }
+
   return json_yang::DumpJson(gnmi_config_json);
 }
 
