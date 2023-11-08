2062a85b6c80dd80cf9ef1c4452178f17c9e7c91
kishanps <kishanps@google.com>
2023-11-08 13:22:26 -0800

    [Thinkit] Add admin state check before checking oper status when checking breakout speeds for interfaces.gNMI port breakout testsAdd an API for returning expected breakout config in JSON format.Fix break out config namespace issue.Update tests to use enum for loopback-modeAdd helper function to get port set with optics and port set with specified PMD type

--- tests/thinkit_gnmi_interface_tests.h
+++ tests/thinkit_gnmi_interface_tests.h
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,15 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GOOGLE_TESTS_THINKIT_GNMI_INTERFACE_TESTS_H_
-#define GOOGLE_TESTS_THINKIT_GNMI_INTERFACE_TESTS_H_
+#ifndef PINS_TESTS_THINKIT_GNMI_INTERFACE_TESTS_H_
+#define PINS_TESTS_THINKIT_GNMI_INTERFACE_TESTS_H_
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/strings/string_view.h"
 #include "p4/config/v1/p4info.pb.h"
 #include "sai_p4/instantiations/google/instantiations.h"
-#include "sai_p4/instantiations/google/sai_p4info.h"
 #include "tests/thinkit_gnmi_interface_util.h"
 #include "thinkit/ssh_client.h"
 #include "thinkit/switch.h"
@@ -30,43 +29,26 @@ namespace pins_test {
 // Test port breakout during parent port in use.
 void TestGNMIParentPortInUseDuringBreakout(
     thinkit::Switch& sut, std::string& platform_json_contents,
-    const p4::config::v1::P4Info& p4_info =
-        sai::GetP4Info(sai::Instantiation::kMiddleblock));
+    const p4::config::v1::P4Info& p4_info);
 
 // Test port breakout during child port in use.
 void TestGNMIChildPortInUseDuringBreakout(
     thinkit::Switch& sut, std::string& platform_json_contents,
-    const p4::config::v1::P4Info& p4_info =
-        sai::GetP4Info(sai::Instantiation::kMiddleblock));
+    const p4::config::v1::P4Info& p4_info);
 
 // Test port breakout on a port while other master port is in use.
 void TestGNMIOtherMasterPortInUseDuringBreakout(
-    thinkit::Switch &sut, std::string &platform_json_contents,
-    const p4::config::v1::P4Info &p4_info);
+    thinkit::Switch& sut, std::string& platform_json_contents,
+    const p4::config::v1::P4Info& p4_info);
 
 // Helper function to test port in use.
-void BreakoutDuringPortInUse(thinkit::Switch &sut,
-                             gnmi::gNMI::StubInterface *sut_gnmi_stub,
+void BreakoutDuringPortInUse(thinkit::Switch& sut,
+                             gnmi::gNMI::StubInterface* sut_gnmi_stub,
                              RandomPortBreakoutInfo port_info,
                              absl::string_view platform_json_contents,
                              absl::string_view port_in_use,
-                             const p4::config::v1::P4Info &p4_info,
+                             const p4::config::v1::P4Info& p4_info,
                              const bool expect_breakout_failure);
 
-// Test port breakout during parent port in use.
-void TestGNMIParentPortInUseDuringBreakout(thinkit::Switch &sut,
-                                           std::string &platform_json_contents);
-
-// Test port breakout during child port in use.
-void TestGNMIChildPortInUseDuringBreakout(thinkit::Switch &sut,
-                                          std::string &platform_json_contents);
-
-// Helper function to test port in use.
-void BreakoutDuringPortInUse(thinkit::Switch &sut,
-                             gnmi::gNMI::StubInterface *sut_gnmi_stub,
-                             RandomPortBreakoutInfo port_info,
-                             const std::string &platform_json_contents,
-                             bool test_child_port_in_use);
-
-} // namespace pins_test
-#endif // GOOGLE_TESTS_THINKIT_GNMI_INTERFACE_TESTS_H_
+}  // namespace pins_test
+#endif  // PINS_TESTS_THINKIT_GNMI_INTERFACE_TESTS_H_
