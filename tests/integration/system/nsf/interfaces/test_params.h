27f9026d0324aaade2bae2cecb71e67a6bd85459
kishanps <kishanps@google.com>
2023-12-19 11:19:42 -0800

    [Thinkit] Create interfaces, structs, and enums to be used in PINs NSF tests.Implement the various traffic generator helpers for PINs NSF tests.

---
+++ tests/integration/system/nsf/interfaces/test_params.h
@@ -0,0 +1,42 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PINS_TESTS_INTEGRATION_SYSTEM_NSF_INTERFACES_TEST_PARAMS_H_
+#define PINS_TESTS_INTEGRATION_SYSTEM_NSF_INTERFACES_TEST_PARAMS_H_
+
+#include <string>
+#include <vector>
+
+#include "tests/integration/system/nsf/interfaces/component_validator.h"
+#include "tests/integration/system/nsf/interfaces/flow_programmer.h"
+#include "tests/integration/system/nsf/interfaces/traffic_helper.h"
+#include "thinkit/generic_testbed_fixture.h"
+
+namespace pins_test {
+
+// Struct to hold test parameters to be injected in PINs NSF integration tests.
+//
+// Note that the `name` is used as the name of the instantiation of the
+// parameterized NSF integration test.
+struct NsfTestParams {
+  std::string name;
+  FlowProgrammer* flow_programmer;
+  TrafficHelper* traffic_helper;
+  thinkit::GenericTestbedInterface* testbed_interface;
+  std::vector<ComponentValidator*> component_validators;
+};
+
+}  // namespace pins_test
+
+#endif  // PINS_TESTS_INTEGRATION_SYSTEM_NSF_INTERFACES_TEST_PARAMS_H_
