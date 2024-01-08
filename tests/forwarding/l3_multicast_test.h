cd5a7735f2b2732a5cc42b9d4b255c6de08dca51
kishanps <kishanps@google.com>
2024-01-08 13:46:30 -0800

    Add tests for multicast programming

--- tests/forwarding/l3_multicast_test.h
+++ tests/forwarding/l3_multicast_test.h
@@ -16,16 +16,13 @@
 #define PINS_TESTS_FORWARDING_L3_MULTICAST_TEST_H_
 
 #include <memory>
-#include <tuple>
+#include <optional>
 
 #include "dvaas/dataplane_validation.h"
-#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "gutil/status_matchers.h"  // IWYU pragma: keep
 #include "p4_pdpi/ir.pb.h"
 #include "p4_pdpi/p4_runtime_session.h"
-#include "tests/lib/switch_test_setup_helpers.h"
-#include "thinkit/mirror_testbed.h"
 #include "thinkit/mirror_testbed_fixture.h"
 
 namespace pins_test {
@@ -38,7 +35,6 @@ struct L3MulticastTestParams {
   // given (default), or otherwise pushes the given config before starting.
   std::optional<std::string> gnmi_config;
   std::optional<p4::config::v1::P4Info> p4info;
-
   std::shared_ptr<dvaas::DataplaneValidator> dvaas;
 };
 
@@ -53,7 +49,6 @@ class L3MulticastTestFixture
   // both switches.
   std::unique_ptr<pdpi::P4RuntimeSession> sut_p4rt_session_;
   std::unique_ptr<pdpi::P4RuntimeSession> control_switch_p4rt_session_;
-
   pdpi::IrP4Info ir_p4info_;
 };
 
