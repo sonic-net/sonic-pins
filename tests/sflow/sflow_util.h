9122fa7a37c7ba0d1759cfa3814dfd2ec6b623b6
kishanps <kishanps@google.com>
2024-02-27 10:31:25 -0800

    Add a check in reboot test to make sure `PORT_INDEX_TABLE` is populated before sending traffic.Enhance `ValidateSutState` to `ValidateTestbedState` by additionally checking Control Switch for `MirrorTestbed`.

--- tests/sflow/sflow_util.h
+++ tests/sflow/sflow_util.h
@@ -24,7 +24,9 @@
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
+#include "absl/types/span.h"
 #include "proto/gnmi/gnmi.grpc.pb.h"
+#include "thinkit/ssh_client.h"
 
 namespace pins {
 
@@ -141,6 +143,11 @@ absl::StatusOr<bool> IsSameIpAddressStr(const std::string& ip1,
 
 // Returns the collector port from `gnmi_config` collector config first
 absl::StatusOr<int> GetSflowCollectorPort(absl::string_view gnmi_config);
+
+// Checks if state db port index table exists for `interfaces`.
+absl::Status CheckStateDbPortIndexTableExists(
+    thinkit::SSHClient& ssh_client, absl::string_view device_name,
+    absl::Span<const std::string> interfaces);
 
 }  // namespace pins
 #endif  // PINS_TESTS_SFLOW_SFLOW_UTIL_H_
