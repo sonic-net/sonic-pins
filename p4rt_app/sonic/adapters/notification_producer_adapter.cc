083b58380599893dc6ecb2daba0c08386acd1d6f
divyagayathri-hcl <divyagayathri.s@hcl.com>
2024-04-22 07:57:42 -0700

    branch2

---
+++ p4rt_app/sonic/adapters/notification_producer_adapter.cc
@@ -0,0 +1,35 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include "p4rt_app/sonic/adapters/notification_producer_adapter.h"
+
+#include <memory>
+
+#include "swss/dbconnector.h"
+#include "swss/notificationproducer.h"
+
+namespace p4rt_app {
+namespace sonic {
+
+NotificationProducerAdapter::NotificationProducerAdapter(
+    swss::DBConnector* db, const std::string& channel)
+    : notification_producer_(
+          std::make_unique<swss::NotificationProducer>(db, channel)) {}
+
+void NotificationProducerAdapter::send(
+    const std::vector<swss::KeyOpFieldsValuesTuple>& kofv) {
+  notification_producer_->experimental_send(kofv);
+}
+
+}  // namespace sonic
+}  // namespace p4rt_app
