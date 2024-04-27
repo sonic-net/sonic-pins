bf45189296ed5fe2d65eb0e53ff62ef0fa3e3f79
bibhuprasad-hcl <161687009+bibhuprasad-hcl@users.noreply.github.com>
2024-04-27 08:32:31 +0530

    [Blackbox Testing Infrastructure] Add helper functions for installing IR table entries on switch. (#48)
    * Add higher-level convenience functions for working with P4RuntimeSession.
    
    PiperOrigin-RevId: 479746832
    
    * Add higher-level convenience functions for working with P4RuntimeSession.
    
    * [Blackbox Testing Infrastructure] Add function for reading IrTableEntries from switch.
    
    PiperOrigin-RevId: 484008645
    
    * [Blackbox Testing Infrastructure] Add helper functions for installing IR table entries on switch.
    
    PiperOrigin-RevId: 484057783
    
    ---------
    
    Co-authored-by: smolkaj <smolkaj@google.com>
    Co-authored-by: jonathan-dilorenzo <dilo@google.com>

--- p4_pdpi/p4_runtime_session_extras_test.cc
+++ p4_pdpi/p4_runtime_session_extras_test.cc
@@ -7,7 +7,6 @@
 #include "gutil/proto_matchers.h"
 #include "p4/config/v1/p4info.pb.h"
 #include "p4/v1/p4runtime.pb.h"
-
 #include "p4_pdpi/ir.h"
 #include "p4_pdpi/ir.pb.h"
 #include "p4_pdpi/p4_runtime_session_mocking.h"
@@ -23,7 +22,6 @@ using ::p4::config::v1::P4Info;
 using ::p4::v1::GetForwardingPipelineConfigResponse;
 using ::p4::v1::WriteRequest;
 using ::testing::ElementsAre;
-
 using ::testing::status::IsOkAndHolds;
 
 TEST(InstallPdTableEntries,
@@ -178,6 +176,108 @@ TEST(InstallPdTableEntry, PullsP4InfoFromSwitchAndWritesPdpiTranslatedEntry) {
     ASSERT_OK(InstallPdTableEntry<pdpi::TableEntry>(*p4rt_session,
                                                     kPdTableEntryString));
   }
+}
+
+TEST(InstallIrTableEntries,
+     PullsP4InfoFromSwitchAndWritesPdpiTranslatedEntries) {
+  // Constants.
+  const P4Info& p4info = GetTestP4Info();
+  const IrP4Info& ir_p4info = GetTestIrP4Info();
+  const P4RuntimeSessionOptionalArgs metadata;
+
+  ASSERT_OK_AND_ASSIGN((auto [p4rt_session, mock_p4rt_stub]),
+                       MakeP4SessionWithMockStub(metadata));
+
+  // Expect function to pull P4Info from switch.
+  EXPECT_CALL(mock_p4rt_stub, GetForwardingPipelineConfig)
+      .WillOnce([&](auto, auto, GetForwardingPipelineConfigResponse* response) {
+        *response->mutable_config()->mutable_p4info() = p4info;
+        return grpc::Status::OK;
+      });
+
+  // PD version of our test entry for readability.
+  ASSERT_OK_AND_ASSIGN(
+      const auto kPdTableEntry,
+      gutil::ParseTextProto<pdpi::TableEntry>(
+          R"pb(
+            ternary_table_entry {
+              match { normal { value: "0x0ff" mask: "0x0ff" } }
+              action { do_thing_3 { arg1: "0xffffffff" arg2: "0xffffffff" } }
+              priority: 1
+            }
+          )pb"));
+
+  // The IR entry that we will install on the switch...
+  ASSERT_OK_AND_ASSIGN(const IrTableEntry kIrTableEntry,
+                       PdTableEntryToIr(ir_p4info, kPdTableEntry));
+
+  // ...and the translated PI entry that we expect will actually be sent to
+  // the switch.
+  ASSERT_OK_AND_ASSIGN(const p4::v1::TableEntry kPiTableEntry,
+                       IrTableEntryToPi(ir_p4info, kIrTableEntry));
+
+  p4::v1::Update expected_update;
+  expected_update.set_type(p4::v1::Update::INSERT);
+  *expected_update.mutable_entity()->mutable_table_entry() = kPiTableEntry;
+
+  // Expect function to issue `Write` request with IR-to-PI-translated entry.
+  EXPECT_CALL(mock_p4rt_stub, Write)
+      .WillOnce([&](auto, const WriteRequest& request, auto) {
+        EXPECT_THAT(request.updates(),
+                    ElementsAre(EqualsProto(expected_update)));
+        return grpc::Status::OK;
+      });
+  ASSERT_OK(InstallIrTableEntries(*p4rt_session, {kIrTableEntry}));
+}
+
+TEST(InstallIrTableEntry, PullsP4InfoFromSwitchAndWritesPdpiTranslatedEntry) {
+  // Constants.
+  const P4Info& p4info = GetTestP4Info();
+  const IrP4Info& ir_p4info = GetTestIrP4Info();
+  const P4RuntimeSessionOptionalArgs metadata;
+
+  ASSERT_OK_AND_ASSIGN((auto [p4rt_session, mock_p4rt_stub]),
+                       MakeP4SessionWithMockStub(metadata));
+
+  // Expect function to pull P4Info from switch.
+  EXPECT_CALL(mock_p4rt_stub, GetForwardingPipelineConfig)
+      .WillOnce([&](auto, auto, GetForwardingPipelineConfigResponse* response) {
+        *response->mutable_config()->mutable_p4info() = p4info;
+        return grpc::Status::OK;
+      });
+
+  // PD version of our test entry for readability.
+  ASSERT_OK_AND_ASSIGN(
+      const auto kPdTableEntry,
+      gutil::ParseTextProto<pdpi::TableEntry>(
+          R"pb(
+            ternary_table_entry {
+              match { normal { value: "0x0ff" mask: "0x0ff" } }
+              action { do_thing_3 { arg1: "0xffffffff" arg2: "0xffffffff" } }
+              priority: 1
+            }
+          )pb"));
+
+  // The IR entry that we will install on the switch...
+  ASSERT_OK_AND_ASSIGN(const IrTableEntry kIrTableEntry,
+                       PdTableEntryToIr(ir_p4info, kPdTableEntry));
+
+  // ...and the translated PI entry that we expect will actually be sent to
+  // the switch.
+  ASSERT_OK_AND_ASSIGN(const p4::v1::TableEntry kPiTableEntry,
+                       IrTableEntryToPi(ir_p4info, kIrTableEntry));
+  p4::v1::Update expected_update;
+  expected_update.set_type(p4::v1::Update::INSERT);
+  *expected_update.mutable_entity()->mutable_table_entry() = kPiTableEntry;
+
+  // Expect function to issue `Write` request with IR-to-PI-translated entry.
+  EXPECT_CALL(mock_p4rt_stub, Write)
+      .WillOnce([&](auto, const WriteRequest& request, auto) {
+        EXPECT_THAT(request.updates(),
+                    ElementsAre(EqualsProto(expected_update)));
+        return grpc::Status::OK;
+      });
+  ASSERT_OK(InstallIrTableEntry(*p4rt_session, kIrTableEntry));
 }
 
 TEST(ReadIrTableEntries, PullsP4InfoFromSwitchAndReadsPdpiTranslatedEntries) {
