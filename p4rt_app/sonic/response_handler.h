5ea56c8da5362837ce6ef1b2e641dfcae1b7a463
divyagayathri-hcl <159437886+divyagayathri-hcl@users.noreply.github.com>
2024-05-08 22:27:27 -0700

    Log performance statistics instead of every read/write flow. (#73)
    * Collect performance statistics in P4RT App.
    Co-authored-by: kishanps <kishanps@google.com>
    Co-authored-by: rhalstea <rhalstea@google.com>

--- p4rt_app/sonic/response_handler.h
+++ p4rt_app/sonic/response_handler.h
@@ -24,15 +24,9 @@
 #include "p4_pdpi/utils/ir.h"
 #include "p4rt_app/sonic/adapters/consumer_notifier_adapter.h"
 #include "p4rt_app/sonic/adapters/table_adapter.h"
-#include "p4rt_app/utils/event_execution_time_monitor.h"
 
 namespace p4rt_app {
 namespace sonic {
-
-enum class ResponseTimeMonitor {
-  kNone,
-  kP4rtTableWrite,
-};
 
 // Given a mapping of keys to IR statuses this function will wait for an
 // OrchAgent response for every key, and update that key's status in the
@@ -44,8 +38,7 @@ enum class ResponseTimeMonitor {
 absl::Status GetAndProcessResponseNotification(
     ConsumerNotifierAdapter& notification_interface, TableAdapter& app_db_table,
     TableAdapter& state_db_table,
-    absl::btree_map<std::string, pdpi::IrUpdateStatus*>& key_to_status_map,
-    ResponseTimeMonitor event_monitor);
+    absl::btree_map<std::string, pdpi::IrUpdateStatus*>& key_to_status_map);
 
 // Given a single key this function will wait for a response from the OrchAgent.
 // If there is no response or that response doesn't match the given key this
@@ -53,8 +46,7 @@ absl::Status GetAndProcessResponseNotification(
 // OrchAgent's status.
 absl::StatusOr<pdpi::IrUpdateStatus> GetAndProcessResponseNotification(
     ConsumerNotifierAdapter& notification_interface, TableAdapter& app_db_table,
-    TableAdapter& state_db_table, const std::string& key,
-    ResponseTimeMonitor event_monitor);
+    TableAdapter& state_db_table, const std::string& key);
 
 // Given a mapping of keys to IR statuses this function will wait for an
 // OrchAgent respsponse for every key and update it's status. On a failed
