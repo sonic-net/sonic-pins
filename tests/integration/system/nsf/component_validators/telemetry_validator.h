a85c3d318ba560bfe63ce080f31b1c2628740ac0
kishanps <kishanps@google.com>
2024-05-15 12:03:49 -0700

    Add telemetry validator for NSF.
    PiperOrigin-RevId: 634032352

---
+++ tests/integration/system/nsf/component_validators/telemetry_validator.h
@@ -0,0 +1,34 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PINS_TESTS_INTEGRATION_SYSTEM_NSF_COMPONENT_VALIDATORS_TELEMETRY_VALIDATOR_H_
+#define PINS_TESTS_INTEGRATION_SYSTEM_NSF_COMPONENT_VALIDATORS_TELEMETRY_VALIDATOR_H_
+
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
+#include "glog/logging.h"
+#include "tests/integration/system/nsf/interfaces/component_validator.h"
+#include "tests/integration/system/nsf/interfaces/testbed.h"
+#include "thinkit/ssh_client.h"
+
+namespace pins_test {
+
+class TelemetryValidator : public ComponentValidator {
+  absl::Status OnNsfReboot(absl::string_view version, Testbed& testbed,
+                           thinkit::SSHClient& ssh_client) override;
+};
+
+}  // namespace pins_test
+
+#endif  // PINS_TESTS_INTEGRATION_SYSTEM_NSF_COMPONENT_VALIDATORS_TELEMETRY_VALIDATOR_H_
