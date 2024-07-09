141bb8eddb4983f2c711e8ff56d2497d5f747276
rkavitha-hcl <158544468+rkavitha-hcl@users.noreply.github.com>
2024-07-09 05:34:36 +0530

    [SAIP4] Pull logic for configuring BMv2 for SAI P4 into reusable helper library     allow ECN matching in the pre-ingress table.     [arriba-lite] Add absl flag parsing/unparsing for ClosStage.     Use packetIn matchers for BMv2 tests and re-use test packetlib::Packet across different tests. (#284)
    * [p4_symbolic]:Initial inport of Ir Table entries and test Code. (#287)
    
    Co-authored-by: bhagatgit <115375369+bhagatgit@users.noreply.github.com>
    
    * [SAIP4] Add ACL ingress security table to Middleblock and update match fields for ToR.
    Reserve 2 entries in the VRF bank.
    
    * Limit ToR ACL pre-ingress table to 127 entries.
    
    * [SAI P4] Model tunnel termination (aka decap).
    
    * [SAIP4] Add ACL ingress security table to Middleblock and update match fields for ToR.
    Reserve 2 entries in the VRF bank.
    
    * [SAI P4] Model tunnel termination (aka decap).
    
    * [SAIP4] Pull logic for configuring BMv2 for SAI P4 into reusable helper library
    allow ECN matching in the pre-ingress table.
    [arriba-lite] Add absl flag parsing/unparsing for ClosStage.
    Use packetIn matchers for BMv2 tests and re-use test packetlib::Packet across different tests.
    
    ---------
    
    Co-authored-by: VSuryaprasad-hcl <159443973+VSuryaprasad-HCL@users.noreply.github.com>
    Co-authored-by: bhagatgit <115375369+bhagatgit@users.noreply.github.com>
    Co-authored-by: jonathan-dilorenzo <dilo@google.com>
    Co-authored-by: smolkaj <smolkaj@google.com>
    Co-authored-by: rhalstea <rhalstea@google.com>

--- sai_p4/instantiations/google/clos_stage.h
+++ sai_p4/instantiations/google/clos_stage.h
@@ -47,6 +47,11 @@ bool DiffersByClosStage(Instantiation instantiation);
 absl::Status AssertInstantiationAndClosStageAreCompatible(
     Instantiation instantiation, std::optional<ClosStage> stage);
 
+bool AbslParseFlag(absl::string_view stage_txt, ClosStage* stage,
+                   std::string* error);
+
+std::string AbslUnparseFlag(ClosStage stage);
+
 }  // namespace sai
 
 #endif  // PINS_SAI_P4_INSTANTIATIONS_GOOGLE_CLOS_STAGE_H_
