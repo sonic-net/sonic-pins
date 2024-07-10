6fd6a8adffb0f51d32490f51ceff305ab4db55f5
kishanps <kishanps@google.com>
2024-07-10 11:32:03 -0700

    Add a test case to verify blackhole LPM miss counter increases when LPM miss count exceeds threshold.
    PiperOrigin-RevId: 651095227

---
+++ tests/gnmi/blackhole_congestion_counters_without_ixia_test.h
@@ -0,0 +1,54 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef PINS_TEST_GNMI_BLACKHOLE_CONGESTION_COUNTERS_WITHOUT_IXIA_TEST_H_
+#define PINS_TEST_GNMI_BLACKHOLE_CONGESTION_COUNTERS_WITHOUT_IXIA_TEST_H_
+
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "lib/utils/generic_testbed_utils.h"
+#include "p4_pdpi/p4_runtime_session.h"
+#include "proto/gnmi/gnmi.grpc.pb.h"
+#include "sai_p4/instantiations/google/test_tools/test_entries.h"
+#include "thinkit/generic_testbed.h"
+#include "thinkit/generic_testbed_fixture.h"
+
+namespace pins_test {
+
+struct LpmMissCounters {
+  uint64_t port_in_packets;
+  uint64_t switch_blackhole_lpm_miss_events;
+  uint64_t switch_blackhole_events;
+};
+
+class BlackholeCongestionCountersWithoutIxiaTestFixture
+    : public thinkit::GenericTestbedFixture<> {
+ protected:
+  void SetUp() override;
+  void TearDown() override;
+  absl::StatusOr<LpmMissCounters> TriggerLpmMisses(sai::IpVersion ip_version,
+                                                   absl::string_view dst_ip,
+                                                   uint32_t packets_count);
+  std::unique_ptr<thinkit::GenericTestbed> generic_testbed_;
+  std::unique_ptr<gnmi::gNMI::StubInterface> gnmi_stub_;
+  std::vector<InterfaceLink> control_links_;
+  std::unique_ptr<pdpi::P4RuntimeSession> sut_p4_session_;
+};
+
+}  // namespace pins_test
+
+#endif  // PINS_TEST_GNMI_BLACKHOLE_CONGESTION_COUNTERS_WITHOUT_IXIA_TEST_H_
