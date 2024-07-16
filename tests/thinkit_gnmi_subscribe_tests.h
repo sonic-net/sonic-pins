bbd46072104f791634bf5041d2bcd94de4cf9eeb
Srikishen Pondicherry Shanmugam <kishanps@google.com>
2024-07-16 15:43:41 +0530

    gNMI Subscribe sanity test

---
+++ tests/thinkit_gnmi_subscribe_tests.h
@@ -0,0 +1,11 @@
+#ifndef GOOGLE_TESTS_THINKIT_GNMI_SUBSCRIBE_TESTS_H_
+#define GOOGLE_TESTS_THINKIT_GNMI_SUBSCRIBE_TESTS_H_
+
+#include "thinkit/switch.h"
+
+namespace pins_test {
+
+void TestGnmiInterfaceAndComponentSubscribe(thinkit::Switch& sut);
+
+}
+#endif  // GOOGLE_TESTS_THINKIT_GNMI_SUBSCRIBE_TESTS_H_
