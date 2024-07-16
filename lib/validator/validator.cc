e3f861ab694f710f2979b1dfb700d393a213ded0
bibhuprasad-hcl <161687009+bibhuprasad-hcl@users.noreply.github.com>
2024-07-16 10:45:12 +0530

    Added CommonBackend for GPINs validator. Add initial implementation of Validator and ValidatorBackend. (#329)
    Co-authored-by: Srikishen Pondicherry Shanmugam <kishanps@google.com>

--- lib/validator/validator.cc
+++ lib/validator/validator.cc
@@ -18,10 +18,13 @@
 #include <utility>
 #include <vector>
 
+#include "absl/algorithm/container.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_join.h"
 #include "absl/strings/string_view.h"
 #include "absl/time/time.h"
 #include "absl/types/span.h"
+#include "glog/logging.h"
 #include "lib/validator/validator_backend.h"
 
 namespace pins_test {
@@ -33,6 +36,39 @@ absl::Status Validator::RunValidations(
     absl::Span<const absl::string_view> devices,
     absl::Span<const absl::string_view> validations, int retry_count,
     absl::Duration timeout) {
+  std::vector<std::string> messages;
+  auto validate_device = [&](absl::string_view device, const auto& backend) {
+    return backend->RunValidations(device, validations, retry_count, timeout);
+  };
+
+  for (const auto& device : devices) {
+    std::vector<absl::Status> validation_statuses;
+    std::vector<std::string> validation_messages;
+    absl::c_transform(
+        backends_, std::back_inserter(validation_statuses),
+        [&](const auto& backend) { return validate_device(device, backend); });
+
+    absl::c_for_each(validation_statuses, [&](const absl::Status& status) {
+      if (!status.ok() && status.code() != absl::StatusCode::kNotFound) {
+        validation_messages.push_back(
+            absl::StrCat(device, " failed with: ", status.message()));
+      }
+    });
+
+    bool any_validation_passed =
+        absl::c_any_of(validation_statuses, std::mem_fn(&absl::Status::ok));
+    // If all statuses failed but there are no messages, then all statuses were
+    // NOT_FOUND.
+    if (!any_validation_passed && validation_messages.empty()) {
+      validation_messages.push_back(
+          absl::StrCat("No backends ran any validations on ", device));
+    }
+    messages.insert(messages.begin(), validation_messages.begin(),
+                    validation_messages.end());
+  }
+  if (!messages.empty()) {
+    return absl::InternalError(absl::StrJoin(messages, "; "));
+  }
   return absl::OkStatus();
 }
 
