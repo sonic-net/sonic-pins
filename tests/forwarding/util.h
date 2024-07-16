6f1c8b91fc10a54b598daf34b824022978c2d30a
divyagayathri-hcl <159437886+divyagayathri-hcl@users.noreply.github.com>
2024-07-15 17:15:37 -0700

    [Dvaas] Adding dvaas/packet_injection files. (#327)
    * [Dvaas] Adding test_run_validation files.
    
    * [Dvaas] Adding dvaas/packet_injection files.
    
    ---------
    
    Co-authored-by: bhagatgit <115375369+bhagatgit@users.noreply.github.com>

--- tests/forwarding/util.h
+++ tests/forwarding/util.h
@@ -12,14 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef GOOGLE_TESTS_FORWARDING_UTIL_H_
-#define GOOGLE_TESTS_FORWARDING_UTIL_H_
+#ifndef PINS_TESTS_FORWARDING_UTIL_H_
+#define PINS_TESTS_FORWARDING_UTIL_H_
 
 #include <functional>
+#include <string>
 
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/time/time.h"
-#include "p4/v1/p4runtime.pb.h"
 #include "p4_pdpi/ir.pb.h"
 #include "p4_pdpi/p4_runtime_session.h"
 
@@ -32,19 +33,41 @@ absl::Status TryUpToNTimes(int n, absl::Duration delay,
                            std::function<absl::Status(int iteration)> callback);
 absl::Status TryUpToNTimes(int n, absl::Duration delay,
                            std::function<absl::Status()> callback);
+template <class T>
+absl::StatusOr<T> TryStatusOrUpToNTimes(
+    int n, absl::Duration delay, std::function<absl::StatusOr<T>()> callback);
 
-// Injects the given test packet via packetIO at the egress port specified by
-// the test packet, using the given P4RT session.
-absl::Status InjectEgressPacket(const std::string& port,
-                                const std::string& packet,
-                                const pdpi::IrP4Info& p4info,
-                                pdpi::P4RuntimeSession* p4rt);
+// Injects the given test packet via packetIO at
+// the egress port specified by the test packet, using the given P4RT session.
+// Providing the optional packet delay argument adds the required
+// fixed delay before injecting the packet.
+absl::Status InjectEgressPacket(
+    const std::string& port, const std::string& packet,
+    const pdpi::IrP4Info& p4info, pdpi::P4RuntimeSession* p4rt,
+    std::optional<absl::Duration> packet_delay = std::nullopt);
 
-// Inject the given packet into the ingress pipeline of the switch.
-absl::Status InjectIngressPacket(const std::string& packet,
-                                 const pdpi::IrP4Info& p4info,
-                                 pdpi::P4RuntimeSession* p4rt);
+// Inject the given packet into the ingress
+// pipeline of the switch.
+// Providing the optional packet delay argument adds the required
+// fixed delay before injecting the packet.
+absl::Status InjectIngressPacket(
+    const std::string& packet, const pdpi::IrP4Info& p4info,
+    pdpi::P4RuntimeSession* p4rt,
+    std::optional<absl::Duration> packet_delay = std::nullopt);
+
+// -- END OF PUBLIC INTERFACE -- implementation details follow -----------------
+
+template <class T>
+absl::StatusOr<T> TryStatusOrUpToNTimes(
+    int n, absl::Duration delay, std::function<absl::StatusOr<T>()> callback) {
+  absl::StatusOr<T> result;
+  TryUpToNTimes(n, delay, [&] {
+    result = callback();
+    return result.status();
+  }).IgnoreError();
+  return result;
+}
 
 }  // namespace gpins
 
-#endif  // GOOGLE_TESTS_FORWARDING_UTIL_H_
+#endif  // PINS_TESTS_FORWARDING_UTIL_H_
