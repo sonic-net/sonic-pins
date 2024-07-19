4760b0a41fb6c49b6001e5620f62e2d47c0d4b8f
Srikishen Pondicherry Shanmugam <kishanps@google.com>
2024-07-19 18:12:29 +0530

    Moved validator to free functions

--- lib/validator/pins_backend.cc
+++ lib/validator/pins_backend.cc
@@ -22,23 +22,12 @@
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
-#include "absl/memory/memory.h"
 #include "absl/status/status.h"
-#include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
-#include "absl/strings/string_view.h"
-#include "absl/time/clock.h"
 #include "absl/time/time.h"
-#include "grpcpp/impl/codegen/client_context.h"
-#include "grpcpp/impl/codegen/status_code_enum.h"
 #include "gutil/status.h"
-#include "lib/gnmi/gnmi_helper.h"
-#include "lib/validator/validator_backend.h"
 #include "p4_pdpi/p4_runtime_session.h"
-#include "p4/v1/p4runtime.grpc.pb.h"
-#include "proto/gnmi/gnmi.grpc.pb.h"
-#include "proto/gnmi/gnmi.pb.h"
-#include "system/system.pb.h"
+#include "lib/validator/validator_lib.h"
 #include "thinkit/switch.h"
 
 namespace pins_test {
@@ -54,67 +43,48 @@ PINSBackend::PINSBackend(std::vector<std::unique_ptr<thinkit::Switch>> switches)
   }
 }
 
-absl::Status PINSBackend::CanEstablishP4RuntimeSession(
-    absl::string_view chassis, absl::Duration timeout) {
+absl::Status PINSBackend::P4rtAble(absl::string_view chassis,
+                                   absl::Duration timeout) {
   auto sut = switches_map_.find(chassis);
   if (sut == switches_map_.end()) {
     return absl::InternalError(
         absl::StrCat("ValidatorBackend passed invalid chassis: ", chassis));
   }
   auto& [sut_name, sut_switch] = *sut;
-  ASSIGN_OR_RETURN(auto p4runtime_stub, sut_switch->CreateP4RuntimeStub());
-  return pdpi::P4RuntimeSession::Create(std::move(p4runtime_stub), sut_switch->DeviceId())
-      .status();
+  return pins_test::P4rtAble(*sut_switch);
 }
 
-absl::Status PINSBackend::CanGetAllInterfaceOverGnmi(absl::string_view chassis,
-                                                     absl::Duration timeout) {
+absl::Status PINSBackend::GnmiAble(absl::string_view chassis,
+                                   absl::Duration timeout) {
   auto sut = switches_map_.find(chassis);
   if (sut == switches_map_.end()) {
     return absl::InternalError(
         absl::StrCat("ValidatorBackend passed invalid chassis: ", chassis));
   }
   auto& [sut_name, sut_switch] = *sut;
-  ASSIGN_OR_RETURN(auto gnmi_stub, sut_switch->CreateGnmiStub());
-  ASSIGN_OR_RETURN(auto req, BuildGnmiGetRequest("", gnmi::GetRequest::ALL));
-
-  gnmi::GetResponse resp;
-  grpc::ClientContext context;
-  context.set_deadline(absl::ToChronoTime(absl::Now() + timeout));
-  context.set_wait_for_ready(true);
-  return gutil::GrpcStatusToAbslStatus(gnmi_stub->Get(&context, req, &resp));
+  return pins_test::GnmiAble(*sut_switch, timeout);
 }
 
-absl::Status PINSBackend::CanGetTimeOverGnoiSystem(absl::string_view chassis,
-                                                   absl::Duration timeout) {
+absl::Status PINSBackend::GnoiAble(absl::string_view chassis,
+                                   absl::Duration timeout) {
   auto sut = switches_map_.find(chassis);
   if (sut == switches_map_.end()) {
     return absl::InternalError(
         absl::StrCat("ValidatorBackend passed invalid chassis: ", chassis));
   }
   auto& [sut_name, sut_switch] = *sut;
-  ASSIGN_OR_RETURN(auto gnoi_system_stub, sut_switch->CreateGnoiSystemStub());
-
-  gnoi::system::TimeRequest request;
-  gnoi::system::TimeResponse response;
-  grpc::ClientContext context;
-  context.set_deadline(absl::ToChronoTime(absl::Now() + timeout));
-  context.set_wait_for_ready(true);
-  return gutil::GrpcStatusToAbslStatus(
-      gnoi_system_stub->Time(&context, request, &response));
+  return pins_test::GnoiAble(*sut_switch, timeout);
 }
 
-absl::Status PINSBackend::CheckAllInterfaceUpOverGnmi(absl::string_view chassis,
-                                                      absl::Duration timeout) {
+absl::Status PINSBackend::PortsUp(absl::string_view chassis,
+                                  absl::Duration timeout) {
   auto sut = switches_map_.find(chassis);
   if (sut == switches_map_.end()) {
     return absl::InternalError(
         absl::StrCat("ValidatorBackend passed invalid chassis: ", chassis));
   }
-
   auto& [sut_name, sut_switch] = *sut;
-  ASSIGN_OR_RETURN(auto gnmi_stub, sut_switch->CreateGnmiStub());
-  return pins_test::CheckAllInterfaceUpOverGnmi(*gnmi_stub);
+  return pins_test::PortsUp(*sut_switch, timeout);
 }
 
 }  // namespace pins_test
