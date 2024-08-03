a7cee0eb1c2b9361ba08356c658e8fae1949adc1
divyagayathri-hcl <divyagayathri.s@hcl.com>
2024-08-02 17:44:39 -0700

    [Thinkit] Adding p4rt port test file.

---
+++ lib/p4rt/p4rt_port_test.cc
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "lib/p4rt/p4rt_port.h"
+
+#include <cstdint>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "gutil/status_matchers.h"
+
+namespace pins_test {
+namespace {
+
+using ::gutil::IsOk;
+using ::gutil::IsOkAndHolds;
+using ::testing::Eq;
+using ::testing::Not;
+
+TEST(P4rtPortId, RoundtripsFromInt) {
+  constexpr int kP4rtPortIdInt = 42;
+  const std::string kP4rtPortIdString = "42";
+  P4rtPortId port_id = P4rtPortId::MakeFromOpenConfigEncoding(kP4rtPortIdInt);
+  EXPECT_EQ(port_id.GetOpenConfigEncoding(), kP4rtPortIdInt);
+  EXPECT_EQ(port_id.GetP4rtEncoding(), kP4rtPortIdString);
+  EXPECT_THAT(P4rtPortId::MakeFromP4rtEncoding(port_id.GetP4rtEncoding()),
+              IsOkAndHolds(Eq(port_id)));
+}
+
+TEST(P4rtPortId, RoundtripsFromString) {
+  constexpr int kP4rtPortIdInt = 42;
+  const std::string kP4rtPortIdString = "42";
+  ASSERT_OK_AND_ASSIGN(P4rtPortId port_id,
+                       P4rtPortId::MakeFromP4rtEncoding(kP4rtPortIdString));
+  EXPECT_EQ(port_id.GetOpenConfigEncoding(), kP4rtPortIdInt);
+  EXPECT_EQ(port_id.GetP4rtEncoding(), kP4rtPortIdString);
+  EXPECT_EQ(
+      P4rtPortId::MakeFromOpenConfigEncoding(port_id.GetOpenConfigEncoding()),
+      port_id);
+}
+
+TEST(P4rtPortId, PointwiseIntEquivalence) {
+  const std::vector<uint32_t> kP4rtPortIdIntSpan = {42, 3, 500};
+  std::vector<P4rtPortId> port_ids_from_vector_function =
+      P4rtPortId::MakeVectorFromOpenConfigEncodings(kP4rtPortIdIntSpan);
+  std::vector<P4rtPortId> port_ids_from_single_function;
+  for (uint32_t port_id : kP4rtPortIdIntSpan) {
+    port_ids_from_single_function.push_back(
+        P4rtPortId::MakeFromOpenConfigEncoding(port_id));
+  }
+  EXPECT_EQ(port_ids_from_vector_function, port_ids_from_single_function);
+}
+
+TEST(P4rtPortId, PointwiseStringEquivalence) {
+  const std::vector<std::string> kP4rtPortIdStringSpan = {"42", "3", "500"};
+  ASSERT_OK_AND_ASSIGN(
+      std::vector<P4rtPortId> port_ids_from_vector_function,
+      P4rtPortId::MakeVectorFromP4rtEncodings(kP4rtPortIdStringSpan));
+  std::vector<P4rtPortId> port_ids_from_single_function;
+  for (absl::string_view port_id_string : kP4rtPortIdStringSpan) {
+    ASSERT_OK_AND_ASSIGN(P4rtPortId port_id,
+                         P4rtPortId::MakeFromP4rtEncoding(port_id_string));
+    port_ids_from_single_function.push_back(port_id);
+  }
+  EXPECT_EQ(port_ids_from_vector_function, port_ids_from_single_function);
+}
+
+TEST(P4rtPortId, InvalidFromString) {
+  EXPECT_THAT(P4rtPortId::MakeFromP4rtEncoding("not a decimal string"),
+              Not(IsOk()));
+  EXPECT_THAT(P4rtPortId::MakeFromP4rtEncoding("-1"), Not(IsOk()));
+  EXPECT_THAT(P4rtPortId::MakeFromP4rtEncoding("99999999999999999999999999999"),
+              Not(IsOk()));
+}
+
+TEST(P4rtPortId, Comparisons) {
+  P4rtPortId port_id_big = P4rtPortId::MakeFromOpenConfigEncoding(42);
+  P4rtPortId port_id_medium = P4rtPortId::MakeFromOpenConfigEncoding(21);
+  P4rtPortId port_id_small = P4rtPortId::MakeFromOpenConfigEncoding(1);
+  EXPECT_LT(port_id_small, port_id_medium);
+  EXPECT_LT(port_id_medium, port_id_big);
+}
+
+}  // namespace
+}  // namespace pins_test
