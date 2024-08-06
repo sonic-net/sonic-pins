4bd34056bfbaca3d8607b86f7295c2bddc692fb1
divyagayathri-hcl <159437886+divyagayathri-hcl@users.noreply.github.com>
2024-08-06 21:17:11 +0530

    Added support for multiple interface modes in InterfaceInfo as hosts are considered to be both Control Device and Traffic Generator from the tests using GenericTestbed point of view. (#423)
    Get peer traffic location while getting traffic generator links.
    Create a utility method to convert speed from bits per second to an OTG layer 1 speed enum.
    
    Co-authored-by: rhalstea <rhalstea@google.com>
    Co-authored-by: kishanps <kishanps@google.com>

--- lib/utils/generic_testbed_utils_test.cc
+++ lib/utils/generic_testbed_utils_test.cc
@@ -16,15 +16,15 @@
 
 #include <memory>
 #include <string>
-#include <tuple>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/memory/memory.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
+#include "artifacts/otg.pb.h"
 #include "gmock/gmock.h"
 #include "grpcpp/support/status.h"
 #include "gtest/gtest.h"
@@ -44,6 +44,8 @@ namespace {
 
 using ::gutil::EqualsProto;
 using ::gutil::IsOkAndHolds;
+using ::gutil::StatusIs;
+using ::otg::Layer1;
 using ::testing::_;
 using ::testing::DoAll;
 using ::testing::Return;
@@ -55,19 +57,23 @@ const auto& GetSutInterfaceInfo() {
   static const auto* const kSutInterfaceInfo =
       new absl::flat_hash_map<std::string, thinkit::InterfaceInfo>(
           {{"Ethernet0",
-            thinkit::InterfaceInfo{.interface_mode = thinkit::DISCONNECTED}},
+            thinkit::InterfaceInfo{.interface_modes = {thinkit::DISCONNECTED}}},
            {"Ethernet8",
-            thinkit::InterfaceInfo{.interface_mode = thinkit::LOOPBACK}},
+            thinkit::InterfaceInfo{.interface_modes = {thinkit::LOOPBACK}}},
            {"Ethernet16",
-            thinkit::InterfaceInfo{.interface_mode = thinkit::CONTROL_INTERFACE,
-                                   .peer_interface_name = "eth-1/1"}},
+            thinkit::InterfaceInfo{
+                .interface_modes = {thinkit::CONTROL_INTERFACE},
+                .peer_interface_name = "eth-1/1"}},
            {"Ethernet24",
             thinkit::InterfaceInfo{
-                .interface_mode = thinkit::TRAFFIC_GENERATOR,
-                .peer_interface_name = "ixia.google.com/1/1"}},
-           {"Ethernet32", thinkit::InterfaceInfo{
-                              .interface_mode = thinkit::TRAFFIC_GENERATOR,
-                              .peer_interface_name = "ixia.google.com/1/2"}}});
+                .interface_modes = {thinkit::TRAFFIC_GENERATOR},
+                .peer_interface_name = "ixia.google.com/1/1",
+                .peer_traffic_location = "ixia.google.com;1;1"}},
+           {"Ethernet32",
+            thinkit::InterfaceInfo{
+                .interface_modes = {thinkit::TRAFFIC_GENERATOR},
+                .peer_interface_name = "ixia.google.com/1/2",
+                .peer_traffic_location = "ixia.google.com;1;2"}}});
   return *kSutInterfaceInfo;
 }
 
@@ -88,12 +94,15 @@ TEST(GenericTestbedUtils, GetPeerInterfacesFromAllControlInterfaces) {
 }
 
 TEST(GenericTestbedUtils, GetAllTrafficGeneratorLinks) {
-  EXPECT_THAT(GetAllTrafficGeneratorLinks(GetSutInterfaceInfo()),
-              UnorderedElementsAre(
-                  InterfaceLink{.sut_interface = "Ethernet24",
-                                .peer_interface = "ixia.google.com/1/1"},
-                  InterfaceLink{.sut_interface = "Ethernet32",
-                                .peer_interface = "ixia.google.com/1/2"}));
+  EXPECT_THAT(
+      GetAllTrafficGeneratorLinks(GetSutInterfaceInfo()),
+      UnorderedElementsAre(
+          InterfaceLink{.sut_interface = "Ethernet24",
+                        .peer_interface = "ixia.google.com/1/1",
+                        .peer_traffic_location = "ixia.google.com;1;1"},
+          InterfaceLink{.sut_interface = "Ethernet32",
+                        .peer_interface = "ixia.google.com/1/2",
+                        .peer_traffic_location = "ixia.google.com;1;2"}));
 }
 
 TEST(GenericTestbedUtils, GetAllLoopbackInterfaces) {
@@ -246,9 +255,22 @@ TEST(GenericTestbedUtils, GetUpInterfaceLinks) {
   // GetAllTrafficGeneratorLinks will return Ethernet24 and Ethernet32, but
   // since Ethernet32 is down, it shouldn't be present.
   EXPECT_THAT(GetUpLinks(GetAllTrafficGeneratorLinks, testbed),
-              IsOkAndHolds(UnorderedElementsAre(
-                  InterfaceLink{.sut_interface = "Ethernet24",
-                                .peer_interface = "ixia.google.com/1/1"})));
+              IsOkAndHolds(UnorderedElementsAre(InterfaceLink{
+                  .sut_interface = "Ethernet24",
+                  .peer_interface = "ixia.google.com/1/1",
+                  .peer_traffic_location = "ixia.google.com;1;1"})));
+}
+
+TEST(OtgHelperTest, GetLayer1SpeedFromBitsPerSecondWorks) {
+  EXPECT_THAT(GetLayer1SpeedFromBitsPerSecond(100000000000),
+              IsOkAndHolds(Layer1::Speed::speed_100_gbps));
+  EXPECT_THAT(GetLayer1SpeedFromBitsPerSecond(200000000000),
+              IsOkAndHolds(Layer1::Speed::speed_200_gbps));
+  EXPECT_THAT(GetLayer1SpeedFromBitsPerSecond(400000000000),
+              IsOkAndHolds(Layer1::Speed::speed_400_gbps));
+  EXPECT_THAT(GetLayer1SpeedFromBitsPerSecond(800000000000),
+              StatusIs(absl::StatusCode::kNotFound,
+                       "Speed not found for bits per second: 800000000000"));
 }
 
 }  // namespace
