467ceba0a9afa8d2e455792994f936edbfd64ba6
kishanps <kishanps@google.com>
2024-11-25 10:58:59 -0800

    Increase multistage_hash_test fidelity by increasing stage 2 packets.

--- tests/forwarding/multistage_hash_test.cc
+++ tests/forwarding/multistage_hash_test.cc
@@ -18,9 +18,12 @@
 #include <utility>
 #include <vector>
 
-#include "absl/container/flat_hash_map.h"
+#include "absl/container/btree_set.h"
 #include "absl/container/flat_hash_set.h"
 #include "absl/container/node_hash_map.h"
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
@@ -29,9 +32,10 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "gutil/status.h"
-#include "gutil/status_matchers.h"  // IWYU pragma: keep
+#include "gutil/status_matchers.h"
 #include "lib/gnmi/gnmi_helper.h"
 #include "lib/p4rt/p4rt_port.h"
+#include "p4_pdpi/p4_runtime_session.h"
 #include "proto/gnmi/gnmi.grpc.pb.h"
 #include "tests/forwarding/group_programming_util.h"
 #include "tests/forwarding/hash_statistics_util.h"
@@ -45,6 +49,37 @@ constexpr char kIpv4Packets[] = "IPv4Packets";
 constexpr char kIpv6Packets[] = "IPv6Packets";
 constexpr absl::string_view kEcmpInterfaceHashAlgPath =
     "interfaces/interface[name=$0]/$1/ecmp-hash-algorithm";
+
+struct WcmpGroup {
+  std::vector<pins::GroupMember> members;
+  int total_weight = 0;
+};
+
+absl::StatusOr<WcmpGroup> CreateWcmpGroup(
+    const absl::btree_set<P4rtPortId>& port_ids, int size) {
+  // Make the first weight the largest since we track packets from that member.
+  // Making the first member the largest means we can get the largest number of
+  // packets.
+  static const auto* const kWeights = new std::vector<int>{8, 7, 6};
+  if (port_ids.size() < size) {
+    return absl::FailedPreconditionError(
+        absl::StrCat("Not enough ports available for test. Needed ", size,
+                     " but got ", port_ids.size(), "."));
+  }
+  WcmpGroup wcmp_group;
+  auto weight = kWeights->begin();
+  auto port_id = port_ids.begin();
+  while (wcmp_group.members.size() < size) {
+    wcmp_group.members.push_back({
+        .weight = *weight,
+        .port = static_cast<int>(port_id->GetOpenConfigEncoding()),
+    });
+    wcmp_group.total_weight += *weight;
+    if (++weight == kWeights->end()) weight = kWeights->begin();
+    ++port_id;
+  }
+  return wcmp_group;
+}
 
 // Returns a hash algorithm that is different than the 'old_hash_alg' but
 // still within the max of 2 hash algorithms allowed per port.
@@ -69,7 +104,7 @@ absl::StatusOr<std::string> GetNewHashAlgorithm(
 }
 
 void VerifyDistribution(
-    const absl::node_hash_map<std::string, int>& num_packets_per_config,
+    const TestPacketMap& test_packet_map,
     const std::vector<pins::GroupMember>& members,
     const absl::node_hash_map<std::string, HashTest::TestData>& hash_test_data,
     absl::string_view test_stage, double confidence, Statistic statistic) {
@@ -84,8 +119,13 @@ void VerifyDistribution(
     }
     SCOPED_TRACE(absl::StrCat("Failed to verify ", config));
     LOG(INFO) << "Results for " << config;
+    double actual_confidence;
     ASSERT_OK(TestDistribution(members, actual_distribution, confidence,
-                               num_packets_per_config.at(config), statistic));
+                               test_packet_map.at(config).size(), statistic,
+                               actual_confidence));
+    testing::Test::RecordProperty(
+        absl::StrCat(test_stage, ".", config, ".confidence"),
+        actual_confidence);
   }
 }
 
@@ -94,28 +134,21 @@ void VerifyDistribution(
 TEST_P(HashMultiStage, MultiStageHashingHasNoPolarizationWithWcmp) {
   ASSERT_OK_AND_ASSIGN(auto sut_gnmi_stub,
                        GetMirrorTestbed().Sut().CreateGnmiStub());
-  // Assign weights for the members with some imbalance, say 6,7,8.
-  const std::vector<int> kWeights = {6, 7, 8, 6, 7, 8, 6, 7, 8, 6};
-  const int kNumMembers = kWeights.size();
 
-  ASSERT_GE(PortIds().size(), kNumMembers)
-      << "Not enough ports are available for this test.";
-  int total_weight = 0;
-  std::vector<pins::GroupMember> members;
-  auto weight = kWeights.begin();
-  for (auto port_id = PortIds().begin();
-       port_id != PortIds().end() && weight != kWeights.end();
-       ++port_id, ++weight) {
-    members.push_back({
-        .weight = *weight,
-        .port = static_cast<int>(port_id->GetOpenConfigEncoding()),
-    });
-    total_weight += *weight;
-  }
+  ASSERT_OK_AND_ASSIGN(WcmpGroup first_stage_group,
+                       CreateWcmpGroup(PortIds(), 4));
+  pins::GroupMember intermediate_member = *first_stage_group.members.begin();
+  P4rtPortId intermediate_port =
+      P4rtPortId::MakeFromOpenConfigEncoding(intermediate_member.port);
+  ASSERT_OK_AND_ASSIGN(WcmpGroup test_group, CreateWcmpGroup(PortIds(), 10));
 
   // Generate IPv4 & IPv6 packets with random field values of all applicable
   // hash fields.
-  const int num_packets = PercentErrorTestPacketCount(total_weight);
+  // The number of packets generated should be large enough that the
+  // intermediate member port receives enough packets for the stage 2 test.
+  const int num_packets = PercentErrorTestPacketCount(test_group.total_weight) *
+                          first_stage_group.total_weight /
+                          intermediate_member.weight;
   ASSERT_OK_AND_ASSIGN(auto ipv4_packets,
                        GeneratePackets(Ipv4HashingOptions(), num_packets));
   ASSERT_OK_AND_ASSIGN(auto ipv6_packets,
@@ -124,30 +157,23 @@ TEST_P(HashMultiStage, MultiStageHashingHasNoPolarizationWithWcmp) {
       {kIpv4Packets, std::move(ipv4_packets)},
       {kIpv6Packets, std::move(ipv6_packets)},
   };
-  absl::node_hash_map<std::string, int> expected_num_packets = {
-      {kIpv4Packets, num_packets},
-      {kIpv6Packets, num_packets},
-  };
 
   // Program groups on SUT, send packets from control switch to sut
   // and capture packets on Control switch to verify distribution.
-  ForwardAllPacketsToMembers(test_p4_info(), members);
+  ForwardAllPacketsToMembers(test_p4_info(), first_stage_group.members);
   const P4rtPortId ingress_port = DefaultIngressPort();
   absl::node_hash_map<std::string, TestData> hash_test_data;
-  {
-    LOG(INFO) << "Sending " << num_packets * 2
-              << " packets to first stage members, ingress port: "
-              << ingress_port.GetP4rtEncoding();
-    ASSERT_OK(SendPacketsAndRecordResultsPerTest(test_packets, test_p4_info(),
-                                                 "first_stage_", ingress_port,
-                                                 hash_test_data));
-  }
-  VerifyDistribution(expected_num_packets, members, hash_test_data,
+  LOG(INFO) << "Sending " << num_packets * 2
+            << " packets to first stage members, ingress port: "
+            << ingress_port.GetP4rtEncoding();
+  ASSERT_OK(SendPacketsAndRecordResultsPerTest(test_packets, test_p4_info(),
+                                               "first_stage_", ingress_port,
+                                               hash_test_data));
+  VerifyDistribution(test_packets, first_stage_group.members, hash_test_data,
                      "first_stage", /*confidence=*/0.85,
                      Statistic::kPercentError);
 
-  // Re-use the first stage ports for second stage too, just change the hash
-  // algorithm for the ingress port.
+  // Change the hash algorithm for the new ingress port.
   ASSERT_OK_AND_ASSIGN(std::string ingress_interface_name,
                        GnmiInterfaceName(ingress_port));
   ASSERT_OK_AND_ASSIGN(
@@ -167,32 +193,27 @@ TEST_P(HashMultiStage, MultiStageHashingHasNoPolarizationWithWcmp) {
             << " from " << ingress_port_old_hash_alg << " to "
             << ingress_port_new_hash_alg;
 
-  // Use packets received on one of the ports (say the last member port) for the
-  // second stage.
-  const P4rtPortId last_port =
-      P4rtPortId::MakeFromOpenConfigEncoding(members.back().port);
+  // Use packets received on the itermediate port for the second stage.
   test_packets.clear();
-  expected_num_packets.clear();
   int total_packets = 0;
   for (const auto& [config_name, data] : hash_test_data) {
-    auto packets = data.GetReceivedPacketsOnPort(last_port);
+    auto packets = data.GetReceivedPacketsOnPort(intermediate_port);
     total_packets += packets.size();
-    expected_num_packets[config_name] = packets.size();
     test_packets[config_name] = std::move(packets);
   }
 
   hash_test_data.clear();
-  {
-    // Send and record results for second stage .
-    LOG(INFO) << "Sending " << total_packets << " packets received from the "
-              << "first stage on last port " << members.back().port
-              << " to second stage members, ingress port: "
-              << ingress_port.GetOpenConfigEncoding();
-    ASSERT_OK(SendPacketsAndRecordResultsPerTest(test_packets, test_p4_info(),
-                                                 "second_stage_", ingress_port,
-                                                 hash_test_data));
-  }
-  VerifyDistribution(expected_num_packets, members, hash_test_data,
+  ASSERT_OK(pdpi::ClearEntities(sut_p4_session()));
+  // Send and record results for second stage .
+  ForwardAllPacketsToMembers(test_p4_info(), test_group.members);
+  LOG(INFO) << "Sending " << total_packets << " packets received from the "
+            << "first stage on intermediate port " << intermediate_port
+            << " to second stage members, ingress port: "
+            << ingress_port.GetOpenConfigEncoding();
+  ASSERT_OK(SendPacketsAndRecordResultsPerTest(test_packets, test_p4_info(),
+                                               "second_stage_", ingress_port,
+                                               hash_test_data));
+  VerifyDistribution(test_packets, test_group.members, hash_test_data,
                      "second_stage", /*confidence=*/0.85,
                      Statistic::kPercentError);
 
