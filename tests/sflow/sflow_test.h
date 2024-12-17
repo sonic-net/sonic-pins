c28f539e611b116be6bf2bc2da0dfff613f3464c
VSuryaprasad-hcl <159443973+VSuryaprasad-HCL@users.noreply.github.com>
2024-12-17 00:58:26 +0000

    [Thinkit] Sflow test basic SetUp (#837)
    Co-authored-by: kishanps <kishanps@google.com>

--- tests/sflow/sflow_test.h
+++ tests/sflow/sflow_test.h
@@ -1,13 +1,67 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef PINS_TESTS_SFLOW_SFLOW_TEST_H_
 #define PINS_TESTS_SFLOW_SFLOW_TEST_H_
 
-#include "thinkit/mirror_testbed_fixture.h"
+#include <memory>
+#include <string>
+#include <thread>  // NOLINT: Need threads (instead of fiber) for upstream code.
+#include <vector>
+
+#include "absl/memory/memory.h"
+#include "gtest/gtest.h"
+#include "p4_pdpi/ir.h"
+#include "p4_pdpi/p4_runtime_session.h"
+#include "proto/gnmi/gnmi.grpc.pb.h"
+#include "thinkit/generic_testbed_fixture.h"
+#include "thinkit/ssh_client.h"
 
 namespace pins {
 
-// TODO: to be implemented
-class SflowTestFixture : public thinkit::MirrorTestbedFixture {
+struct SflowTestParams {
+  thinkit::GenericTestbedInterface* testbed_interface;
+  thinkit::SSHClient* ssh_client;
+  std::string gnmi_config;
+  p4::config::v1::P4Info p4_info;
+};
+
+// Structure represents a link between SUT and Ixia.
+// This is represented by Ixia interface name and the SUT's gNMI interface
+// name and its corrosponding p4 runtime id.
+struct IxiaLink {
+  std::string ixia_interface;
+  std::string sut_interface;
+  int port_id;
+};
+
+class SflowTestFixture : public testing::TestWithParam<SflowTestParams> {
  protected:
+  void SetUp() override;
+
+  void TearDown() override;
+
+  const p4::config::v1::P4Info& GetP4Info() { return GetParam().p4_info; }
+  const pdpi::IrP4Info& GetIrP4Info() { return ir_p4_info_; }
+
+  std::unique_ptr<thinkit::GenericTestbed> testbed_;
+  pdpi::IrP4Info ir_p4_info_;
+  std::unique_ptr<gnmi::gNMI::StubInterface> gnmi_stub_;
+  std::unique_ptr<pdpi::P4RuntimeSession> sut_p4_session_;
+  thinkit::SSHClient* ssh_client_ = GetParam().ssh_client;
+
+  std::vector<IxiaLink> ready_links_;
 };
 
 }  // namespace pins
