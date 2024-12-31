0bd8e46a8db28a3653f56cbbd0e27cb63af34b31
Jonathan Dilorenzo <dilo@google.com>
2024-12-31 08:08:56 +0000

    [Thinkit] Adding match_action_coverage files.

---
+++ tests/forwarding/match_action_coverage_test.h
@@ -0,0 +1,33 @@
+#ifndef PINS_TESTS_FORWARDING_MATCH_ACTION_COVERAGE_TEST_H_
+#define PINS_TESTS_FORWARDING_MATCH_ACTION_COVERAGE_TEST_H_
+
+#include <string>
+
+#include "p4_fuzzer/fuzzer_config.h"
+#include "thinkit/mirror_testbed_fixture.h"
+
+namespace p4_fuzzer {
+
+struct MatchActionCoverageTestParams {
+  thinkit::MirrorTestbedInterface* testbed_interface;
+  p4::config::v1::P4Info p4info;
+  FuzzerConfig config;
+};
+
+// This test installs at least one table entry using (or not using if possible)
+// each match field and each action in each possible table.
+// Pushes the input `p4info` to the switch. Assumes that the switch is already
+// configured with a gNMI config.
+class MatchActionCoverageTestFixture
+    : public testing::TestWithParam<MatchActionCoverageTestParams> {
+ protected:
+  void SetUp() override { GetParam().testbed_interface->SetUp(); }
+  void TearDown() override { GetParam().testbed_interface->TearDown(); }
+  ~MatchActionCoverageTestFixture() override {
+    delete GetParam().testbed_interface;
+  }
+};
+
+}  // namespace p4_fuzzer
+
+#endif  // PINS_TESTS_FORWARDING_MATCH_ACTION_COVERAGE_TEST_H_
