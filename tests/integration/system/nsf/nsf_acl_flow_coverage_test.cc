e5680d553f71d9b96f449ac1e476bf270bdcc395
kishanps <kishanps@google.com>
2025-09-08 17:18:28 +0530

    Include compare_p4flows_test to NSF test suite.

--- tests/integration/system/nsf/nsf_acl_flow_coverage_test.cc
+++ tests/integration/system/nsf/nsf_acl_flow_coverage_test.cc
@@ -23,6 +23,7 @@
 #include "gutil/status_matchers.h"
 #include "sai_p4/instantiations/google/sai_pd.pb.h"
 #include "tests/integration/system/nsf/interfaces/image_config_params.h"
+#include "tests/integration/system/nsf/compare_p4flows.h"
 #include "tests/integration/system/nsf/interfaces/testbed.h"
 #include "tests/integration/system/nsf/util.h"
 #include "thinkit/switch.h"
