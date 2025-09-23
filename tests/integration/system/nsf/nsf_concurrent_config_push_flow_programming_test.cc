b78ead5ffbaa6dc4a10ddc30eab9d9488945203f
kishanps <kishanps@google.com>
2025-09-23 12:30:18 +0000

    Introduce random delay before triggering NSF reboot in concurrent config push and flow programming test.

--- tests/integration/system/nsf/nsf_concurrent_config_push_flow_programming_test.cc
+++ tests/integration/system/nsf/nsf_concurrent_config_push_flow_programming_test.cc
@@ -18,7 +18,9 @@
 #include <thread>
 #include <vector>
 
+#include "absl/random/random.h"
 #include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
 #include "absl/time/clock.h"
 #include "absl/time/time.h"
 #include "glog/logging.h"
@@ -28,8 +30,8 @@
 #include "gutil/status_matchers.h"
 #include "lib/gnmi/gnmi_helper.h"
 #include "sai_p4/instantiations/google/sai_pd.pb.h"
-#include "tests/integration/system/nsf/interfaces/image_config_params.h"
 #include "tests/integration/system/nsf/compare_p4flows.h"
+#include "tests/integration/system/nsf/interfaces/image_config_params.h"
 #include "tests/integration/system/nsf/interfaces/testbed.h"
 #include "tests/integration/system/nsf/util.h"
 #include "thinkit/switch.h"
@@ -39,7 +41,14 @@ namespace pins_test {
 using ::p4::v1::ReadResponse;
 
 constexpr int kIsolatedLacpSystemPriority = 512;
-constexpr absl::Duration kNsfThreadDelay = absl::Seconds(1);
+// Here is the criteria for the NSF delay duration window:
+// 1. ACL flows used in the test takes ~1s to get programmed. Hence the minimum
+//    delay is set to 1s.
+// 2. Config push takes ~10s to complete. Hence the maximum delay is set to 15s.
+// In future, if the config or the flows are modified, consider calculating the
+// time needed for both and set the NSF delay duration window accordingly.
+constexpr int kMinNsfDelayDuration = 1;
+constexpr int kMaxNsfDelayDuration = 15;
 constexpr absl::Duration kTurnUpTimeout = absl::Minutes(6);
 constexpr char kInterfaceToRemove[] = "Ethernet1/10/1";
 
@@ -103,64 +112,77 @@ TEST_P(NsfConcurrentConfigPushFlowProgrammingTestFixture,
   ASSERT_OK(environment.StoreTestArtifact(
       "modified_gnmi_config.json", modified_image_config_param.gnmi_config));
 
-  std::vector<std::thread> threads;
+  ASSERT_OK(StoreSutDebugArtifacts(
+      absl::StrCat(
+          "before_nsf_reboot_",
+          absl::FormatTime("%H_%M_%S", absl::Now(), absl::LocalTimeZone())),
+      sut, environment));
+  absl::BitGen gen;
+  int nsf_delay_duration = absl::uniform_int_distribution<int>(
+      kMinNsfDelayDuration, kMaxNsfDelayDuration)(gen);
+
   // Config Push thread.
   absl::Status config_push_status = absl::UnknownError("Yet to push config");
+  auto config_push_func = [&sut,
+                           &modified_image_config_param]() -> absl::Status {
+    LOG(INFO) << "Pushing modified config on " << sut.ChassisName();
+    RETURN_IF_ERROR(
+        PushGnmiConfig(sut, modified_image_config_param.gnmi_config));
+    LOG(INFO) << "Successfully pushed modified config on " << sut.ChassisName();
+    return absl::OkStatus();
+  };
   std::thread config_push_thread(
-      [&config_push_status, &sut, &modified_image_config_param] {
-        LOG(INFO) << "Pushing modified config on " << sut.ChassisName();
-        config_push_status =
-            PushGnmiConfig(sut, modified_image_config_param.gnmi_config);
-        if (config_push_status.ok()) {
-          LOG(INFO) << "Completed pushing modified config";
-        } else {
-          LOG(INFO) << "Failed to push modified config: " << config_push_status;
-        }
-        return config_push_status;
-      });
+      [&]() { config_push_status = config_push_func(); });
 
   // Flow Programming thread.
   absl::Status flow_programming_status =
       absl::UnknownError("Yet to program flows");
-  ReadResponse p4flow_snapshot2;
-  std::thread flow_programming_thread([&flow_programming_status, &sut,
-                                       &environment, &image_config_param,
-                                       &p4flow_snapshot2]() -> absl::Status {
+  auto flow_programming_func = [&sut, &image_config_param]() -> absl::Status {
     LOG(INFO) << "Programming ACL flows";
     RETURN_IF_ERROR(ProgramAclFlows(sut, image_config_param.p4_info));
-    // P4 snapshot before NSF reboot.
-    LOG(INFO) << "Capturing P4 snapshot before NSF reboot";
-    ASSIGN_OR_RETURN(p4flow_snapshot2, TakeP4FlowSnapshot(sut));
-    flow_programming_status = SaveP4FlowSnapshot(
-        p4flow_snapshot2, "p4flow_snapshot2_before_nsf.txt", environment);
-    if (flow_programming_status.ok()) {
-      LOG(INFO) << "Completed programming ACL flows";
-    } else {
-      LOG(INFO) << "Failed to program ACL flows: " << flow_programming_status;
-    }
-    return flow_programming_status;
-  });
+    LOG(INFO) << "Successfully programmed ACL flows on " << sut.ChassisName();
+    return absl::OkStatus();
+  };
+  std::thread flow_programming_thread(
+      [&]() { flow_programming_status = flow_programming_func(); });
 
   // NSF Reboot thread.
   absl::Status nsf_reboot_status = absl::UnknownError("Yet to do nsf reboot");
-  std::thread nsf_reboot_thread([&flow_programming_status, &config_push_status,
-                                 &nsf_reboot_status, this]() -> absl::Status {
+  ReadResponse p4flow_snapshot2;
+  auto nsf_reboot_func = [&nsf_delay_duration, &sut, &environment,
+                          &p4flow_snapshot2]() -> absl::Status {
+    // Sleeping for few seconds before joining NSF thread. This is to make sure
+    // config push and flow programming are initiated before NSF.
+    LOG(INFO) << "Sleeping for " << nsf_delay_duration
+              << " seconds before triggering NSF reboot";
+    absl::SleepFor(absl::Seconds(nsf_delay_duration));
+    // P4 snapshot before NSF reboot.
+    LOG(INFO) << "Capturing P4 snapshot before NSF reboot";
+    ASSIGN_OR_RETURN(p4flow_snapshot2, TakeP4FlowSnapshot(sut));
     LOG(INFO) << "Initiating NSF reboot";
-    nsf_reboot_status = pins_test::NsfReboot(testbed_);
-    if (nsf_reboot_status.ok()) {
-      LOG(INFO) << "Successfully initiated NSF reboot";
-    }
-    return nsf_reboot_status;
-  });
+    // Using generic Reboot helper instead of Nsf specific helper as that has an
+    // initial delay to collect the debug artifacts. The below call will trigger
+    // NSF reboot immediately.
+    RETURN_IF_ERROR(
+        pins_test::Reboot(gnoi::system::RebootMethod::NSF, sut, environment,
+                          /*collect_debug_artifacts_before_reboot=*/false));
+    LOG(INFO) << "Successfully triggered NSF reboot on " << sut.ChassisName();
+    return absl::OkStatus();
+  };
+  std::thread nsf_reboot_thread(
+      [&]() { nsf_reboot_status = nsf_reboot_func(); });
+
   // Joining all threads.
   config_push_thread.join();
   flow_programming_thread.join();
-  // Sleeping for a second before joining NSF thread. This is to make sure
-  // config push and flow programming are initiated before NSF.
-  absl::SleepFor(kNsfThreadDelay);
   nsf_reboot_thread.join();
-  ASSERT_OK(config_push_status) << "Failed to push config";
-  ASSERT_OK(flow_programming_status) << "Failed to program flows";
+  // Calling the SaveP4FlowSnapshot function after the NSF reboot thread is
+  // joined. This is to avoid the delay that might get introduced by the
+  // SaveP4FlowSnapshot function before the NSF reboot.
+  EXPECT_OK(SaveP4FlowSnapshot(p4flow_snapshot2,
+                               "p4flow_snapshot2_before_nsf.txt", environment));
+  EXPECT_OK(config_push_status) << "Failed to push config";
+  EXPECT_OK(flow_programming_status) << "Failed to program flows";
   ASSERT_OK(nsf_reboot_status) << "Failed to initiate NSF reboot";
   ASSERT_OK(
       WaitForNsfReboot(testbed_, *ssh_client_, &modified_image_config_param));
