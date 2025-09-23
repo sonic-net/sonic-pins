b19acb1da2369eed1217f901f686c4b5d8b0942c
kishanps <kishanps@google.com>
2025-09-23 02:35:11 +0000

    Optimize NSF link flap verification by adding timeout and return total time taken to flap all the links by using FlapAllLinksAndReportDuration.

--- tests/integration/system/nsf/nsf_link_flap_test.cc
+++ tests/integration/system/nsf/nsf_link_flap_test.cc
@@ -15,18 +15,23 @@
 
 #include <memory>
 #include <string>
+#include <thread>
 #include <utility>
+#include <vector>
 
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
+#include "absl/status/status.h"
 #include "absl/strings/string_view.h"
 #include "absl/time/clock.h"
 #include "absl/time/time.h"
 #include "glog/logging.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "gutil/status.h"
 #include "gutil/status_matchers.h"  // NOLINT: Need to add status_matchers.h for using `ASSERT_OK` in upstream code.
 #include "gutil/testing.h"
+#include "proto/gnmi/gnmi.grpc.pb.h"
 #include "tests/gnmi/util.h"
 #include "tests/integration/system/nsf/interfaces/image_config_params.h"
 #include "tests/integration/system/nsf/interfaces/testbed.h"
@@ -38,8 +43,77 @@
 namespace pins_test {
 namespace {
 
-constexpr double kLinkFlapThresholdPostNsfReboot = 0.05;
+constexpr absl::Duration kLinkFlapTimeout = absl::Seconds(60);
 
+// Flaps all the links in parallel and returns the total time taken to flap all
+// the links.
+absl::StatusOr<absl::Duration> FlapAllLinksAndReportDuration(
+    gnmi::gNMI::StubInterface& gnmi_stub,
+    thinkit::GenericTestbed& generic_testbed,
+    absl::flat_hash_map<std::string, thinkit::InterfaceInfo>
+        host_interface_infos) {
+  // Create a flat_hash_map to store the link flap results for all the links
+  // where the key is the SUT link name and the value is the link flap
+  // status.
+  absl::flat_hash_map<std::string, absl::Status> link_flap_status;
+
+  // Initialize the link_flap_status with all the links. This is to avoid
+  // re-hashing and thus avoid the race condition and link_flap_status can be
+  // used for concurrent writes.
+  for (const auto& [sut_interface, host_interface_info] :
+       host_interface_infos) {
+    link_flap_status.emplace(sut_interface, absl::Status());
+  }
+
+  // Create a vector of threads to flap all the links in parallel.
+  std::vector<std::thread> link_threads;
+
+  // Start the link flap for all the interfaces in parallel.
+  const absl::Time link_flap_start_time = absl::Now();
+  for (const auto& [sut_interface, host_interface_info] :
+       host_interface_infos) {
+    // Flap all the links in parallel.
+    std::thread thread([&gnmi_stub, &generic_testbed, &sut_interface,
+                        &host_interface_info, &link_flap_status] {
+      link_flap_status[sut_interface] = FlapLink(
+          gnmi_stub, generic_testbed, sut_interface, host_interface_info);
+    });
+    link_threads.push_back(std::move(thread));
+  }
+
+  // Wait for all the threads to complete.
+  for (std::thread& thread : link_threads) {
+    thread.join();
+  }
+
+  // Calculate the total time taken to flap all the links.
+  const absl::Duration link_flap_duration = absl::Now() - link_flap_start_time;
+
+  // Check if any link flaps have failed.
+  absl::Status overall_status;
+  for (const auto& [sut_interface, status] : link_flap_status) {
+    if (status.ok()) continue;
+    absl::Status annotated_status = gutil::StatusBuilder(status).SetPrepend()
+                                    << "Failed to flap link: " << sut_interface
+                                    << " due to error " << status << ": ";
+    if (overall_status.ok()) {
+      overall_status.Update(annotated_status);
+    } else {
+      overall_status = gutil::StatusBuilder(overall_status).SetAppend()
+                       << "\n"
+                       << annotated_status.ToString();
+    }
+  }
+
+  // Return the overall status if any link flap has failed.
+  if (!overall_status.ok()) {
+    return overall_status;
+  }
+
+  // Return the total time taken to flap all the links.
+  LOG(INFO) << "Link flap duration: " << link_flap_duration;
+  return link_flap_duration;
+}
 }  // namespace
 
 TEST_P(NsfLinkFlapTestFixture, NsfLinkFlapTest) {
@@ -78,40 +152,44 @@ TEST_P(NsfLinkFlapTestFixture, NsfLinkFlapTest) {
           << interface << " as interface mode is not CONTROL_INTERFACE.";
     }
   }
+  if (host_interface_infos.empty()) {
+    GTEST_SKIP() << "No suitable link found in SUT for link flap.";
+  }
+
   ASSERT_OK_AND_ASSIGN(auto gnmi_stub, generic_testbed->Sut().CreateGnmiStub());
 
   LOG(INFO) << "Flap links before NSF Reboot.";
-  // Capturing the current time to compare the overall time needed for link
-  // flap.
-  const absl::Time pre_nsf_link_flap_start_time = absl::Now();
-  for (const auto& [sut_interface, host_interface_info] :
-       host_interface_infos) {
-    EXPECT_OK(FlapLink(*gnmi_stub, *generic_testbed, sut_interface,
-                       host_interface_info));
+
+  // Flap links and report the time taken to flap all the links.
+  const absl::StatusOr<absl::Duration> pre_nsf_link_flap_time =
+      FlapAllLinksAndReportDuration(*gnmi_stub, *generic_testbed,
+                                    host_interface_infos);
+  if (pre_nsf_link_flap_time.ok()) {
+    LOG(INFO) << "Pre-NSF Link Flap Time: " << pre_nsf_link_flap_time.value();
+    EXPECT_LE(pre_nsf_link_flap_time.value(), kLinkFlapTimeout)
+        << "Pre-NSF Link Flap Time: " << pre_nsf_link_flap_time.value()
+        << " is greater than the maximum expected time: " << kLinkFlapTimeout;
+  } else {
+    // Fail the test if the link flap before NSF fails.
+    ADD_FAILURE() << pre_nsf_link_flap_time.status().message();
   }
-  const absl::Duration pre_nsf_link_flap_time =
-      absl::Now() - pre_nsf_link_flap_start_time;
-  LOG(INFO) << "Pre-NSF Link Flap Time: " << pre_nsf_link_flap_time;
   LOG(INFO) << "Initiating NSF reboot";
   ASSERT_OK(DoNsfRebootAndWaitForSwitchReady(generic_testbed.get(),
                                              *GetParam().ssh_client));
   LOG(INFO) << "Flap links after NSF Reboot.";
-  const absl::Time post_nsf_link_flap_start_time = absl::Now();
-  for (const auto& [sut_interface, host_interface_info] :
-       host_interface_infos) {
-    EXPECT_OK(FlapLink(*gnmi_stub, *generic_testbed, sut_interface,
-                       host_interface_info));
-  }
-  const absl::Duration post_nsf_link_flap_time =
-      absl::Now() - post_nsf_link_flap_start_time;
-  LOG(INFO) << "Post-NSF Link Flap Time: " << post_nsf_link_flap_time;
 
-  if (post_nsf_link_flap_time > pre_nsf_link_flap_time) {
-    double link_flap_time_increase =
-        absl::FDivDuration((post_nsf_link_flap_time - pre_nsf_link_flap_time),
-                           pre_nsf_link_flap_time);
-    EXPECT_LE(link_flap_time_increase, kLinkFlapThresholdPostNsfReboot)
-        << "Link flap time has increased post NSF";
+  // Flap links and report the time taken to flap all the links.
+  const absl::StatusOr<absl::Duration> post_nsf_link_flap_time =
+      FlapAllLinksAndReportDuration(*gnmi_stub, *generic_testbed,
+                                    host_interface_infos);
+  if (post_nsf_link_flap_time.ok()) {
+    LOG(INFO) << "Post-NSF Link Flap Time: " << post_nsf_link_flap_time.value();
+    EXPECT_LE(post_nsf_link_flap_time.value(), kLinkFlapTimeout)
+        << "Post-NSF Link Flap Time: " << post_nsf_link_flap_time.value()
+        << " is greater than the maximum expected time: " << kLinkFlapTimeout;
+  } else {
+    // Fail the test if the link flap after NSF fails.
+    ADD_FAILURE() << post_nsf_link_flap_time.status().message();
   }
 }
 
